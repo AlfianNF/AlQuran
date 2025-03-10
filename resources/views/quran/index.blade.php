89d0309202d4fbcbfd79acf6acba62cd25ed1933
AlfianNF <alfiannurfariq@gmail.com>
2025-03-10 10:44:08 +0700

    memperbaiki tampilan sedikit pada halaman show

--- resources/views/quran/index.blade.php
+++ resources/views/quran/index.blade.php
@@ -2,23 +2,41 @@
 <html>
 <head>
     <title>Daftar Surah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Daftar Surah</h1>
+    <div class="container">
+        <h1 class="text-center">Daftar Surah</h1>
 
-    @if (isset($error))
-        <p style="color: red;">{{ $error }}</p>
-    @endif
+        @if (isset($error))
+            <p class="text-danger">{{ $error }}</p>
+        @endif
 
-    <ul>
-        @foreach ($surahList as $surah)
-            <li>
-                <a href="{{ route('surat.show', ['surat' => $surah->number, 'jumlahAyat' => $surah->number_of_verses]) }}">
-                    <strong>{{ $surah->name_id }}</strong> ({{ $surah->name_en }}) -
-                    Surat: {{ $surah->number }}, Ayat: {{ $surah->number_of_verses }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nomor Surah</th>
+                    <th>Nama Surah (Indonesia)</th>
+                    <th>Nama Surah (Inggris)</th>
+                    <th>Jumlah Ayat</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($surahList as $surah)
+                    <tr>
+                        <td>{{ $surah->number }}</td>
+                        <td>{{ $surah->name_id }}</td>
+                        <td>{{ $surah->name_en }}</td>
+                        <td>{{ $surah->number_of_verses }}</td>
+                        <td>
+                            <a href="{{ route('surat.show', ['surat' => $surah->number, 'jumlahAyat' => $surah->number_of_verses]) }}" class="btn btn-primary btn-sm">Lihat Ayat</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/quran/surat.blade.php
+++ resources/views/quran/surat.blade.php
@@ -7,16 +7,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Daftar Ayat Surat</h1>
+    <h1 class="text-center">Daftar Ayat Surat</h1>
 
     @if ($ayatList->isNotEmpty())
         @foreach ($ayatList as $ayat)
             <div>
-                <h2>Ayat {{ $ayat['ayah'] }}</h2>
-                <p><strong>Arab:</strong> {{ $ayat['arab'] }}</p>
-                <p><strong>Latin:</strong> {{ $ayat['latin'] }}</p>
-                <p><strong>Terjemahan:</strong> {{ $ayat['text'] }}</p>
-                <audio controls>
+                <p class="text-end px-5"> {{ $ayat['arab'] }}</p>
+                <p class="text-end px-5"> {{ $ayat['latin'] }}</p>
+                <p class="text-start px-5"><strong>{{ $ayat['ayah'] }}.</strong> {{ $ayat['text'] }}</p>
+                <audio controls class="text-start px-5">
                     <source src="{{ $ayat['audio'] }}" type="audio/mpeg">
                     Browser Anda tidak mendukung elemen audio.
                 </audio>
@@ -24,7 +23,6 @@
             <hr>
         @endforeach
 
-        {{-- Tampilkan pagination --}}
         <div>
             {{ $ayatList->links() }}
         </div>
